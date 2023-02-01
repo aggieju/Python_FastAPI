a96618c5827c631d7157880477b928630a6a9c48
aggieju <a.juszczuk86@gmail.com>
2023-02-01 20:48:25 +0100

    defining variables and functions

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 print("Hello, Python")
+
+
+# declaring variables
+
+name = "Guido van Rossum"
+age = 67
+created_python = True
+
+print(name, age, created_python)
+print(type(name), type(age), type(created_python))
+
+# creating functions 
+
+def my_function():
+  print("Hello, function")
+
+my_function()
+
+def say_hi(name):
+  print("Hi, " + name)
+
+say_hi("AggieS")
+
+def add(x, y):
+  return x + y
